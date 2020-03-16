730bf069b6f7fbac5dce23a0e3fa40c35cf04ecb
Ian Mostar <ian@rallyhr.com>
2020-03-16 23:57:43 +0800

    add build files

---
+++ build/precache-manifest.80187d70a1d63ef0717cc9bb6ae5a682.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f5adcd0c5c6dcb917aa059434f07f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bd145c72784aecb77c0",
+    "url": "/static/js/2.5eda00bb.chunk.js"
+  },
+  {
+    "revision": "b5ab12cb613b9b32321f",
+    "url": "/static/js/main.5c38f789.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b7904883d5b7fd4efd102f2fc845df8",
+    "url": "/static/media/arrow_down.6b790488.svg"
+  },
+  {
+    "revision": "3026f0802816214a46c7548d8b3d55ff",
+    "url": "/static/media/codepen.3026f080.svg"
+  },
+  {
+    "revision": "15873a05aa4180cd37288c62e0c170b7",
+    "url": "/static/media/email.15873a05.svg"
+  },
+  {
+    "revision": "bddbe5c291f39316c7ac76ea154b403e",
+    "url": "/static/media/github.bddbe5c2.svg"
+  },
+  {
+    "revision": "9ede09f7182cb615fe3743bf3dd612b5",
+    "url": "/static/media/linkedin.9ede09f7.svg"
+  },
+  {
+    "revision": "c2fd78d0cc87b9f03637aba87eedf8e5",
+    "url": "/static/media/twitter.c2fd78d0.svg"
+  },
+  {
+    "revision": "70946c355999f3863535b83ff500495b",
+    "url": "/static/media/work_1.70946c35.png"
+  },
+  {
+    "revision": "e6bb783811bd6676a413ed780d412882",
+    "url": "/static/media/work_2.e6bb7838.png"
+  }
+]);

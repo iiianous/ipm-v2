bea3707cf778f933f78b631b98129d1b3694c454
Ian Mostar <ian@rallyhr.com>
2020-03-20 09:47:53 +0800

    update about

--- src/components/SectionAbout.js
+++ src/components/SectionAbout.js
@@ -29,16 +29,16 @@ const SectionAbout = () => {
         about
       </SubHead>
       <Text>
-        I'm Ian Mostar, based in Cebu, Philippines. I have been working in web development for 5 years. The first 2 years, I mostly work as a web & mobile UI designer. I've transitioned more into a front-end developer role creating web applications and small business websites using custom Wordpress CMS.
+        I'm Ian Mostar, based in Cebu, Philippines. I have been working in web development for 5 years. The first 2 years, I mostly work as a web & mobile UI designer. I've transitioned more into a front-end developer creating small business websites using custom Wordpress CMS.
         <br />
         <br />
-        I work with a team developing various types of web applications such as HR platform, credential certificates and betting platform.{/*<a href={pdf_src} target="_blank">[resume]</a>*/}
+        I work with a team developing various types of web applications such as HR management, credential certificates and betting platforms.{/*<a href={pdf_src} target="_blank">[resume]</a>*/}
         <br />
         <br />
         I create frontend experiences using Vue.js/Vuex, React.js/Redux, Javascript, TailwindCSS, CSS3/SCSS/HTML5.
         <br />
         <br />
-        You'll find me on <a href="//twitter.com/iiianous" target="_blank">Twitter</a>, dabbling latest and hot-picking design and web development posts around.
+        You'll find me on <a href="//twitter.com/iiianous" target="_blank">Twitter</a>, hot-picking design and web development posts around.
       </Text>
     </Container>
   )}

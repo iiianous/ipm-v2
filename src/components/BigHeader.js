832a2ed489c1cfd60029927d857a6e55f39a5bf3
Ian Mostar <ian@rallyhr.com>
2020-05-26 11:54:19 +0800

    update top position

--- src/components/BigHeader.js
+++ src/components/BigHeader.js
@@ -1,37 +1,36 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const BigHeader = styled('h1')`
-  font-family: 'Lato', sans-serif;
-  font-size: 3.6rem;
-  text-align: right;
-  font-weight: 900;
-  z-index: 1;
-  line-height: .75;
-  align-self: center;
-  letter-spacing: -0.02em;
-  text-transform: uppercase;
-  top: -1px;
-  position: relative;
-  color: var(--font-color);
+const BigHeader = styled("h1")`
+	font-family: "Lato", sans-serif;
+	font-size: 3.6rem;
+	text-align: right;
+	font-weight: 900;
+	z-index: 1;
+	line-height: 0.75;
+	align-self: center;
+	letter-spacing: -0.02em;
+	text-transform: uppercase;
+	top: -9px;
+	position: relative;
+	color: var(--font-color);
 
-  @media only screen and ( min-width: 600px) {
-    font-size: 5.2rem;
-  }
+	@media only screen and (min-width: 600px) {
+		font-size: 5.2rem;
+	}
 
+	@media only screen and (min-width: 768px) {
+		font-size: 6.2rem;
+	}
 
-  @media only screen and ( min-width: 768px) {
-    font-size: 6.2rem;
-  }
+	@media only screen and (min-width: 992px) {
+		font-size: 7.8rem;
+	}
 
-  @media only screen and ( min-width: 992px) {
-    font-size: 7.8rem;
-  }
-
-  span {
-    position: relative;
-    display: inline-block;
-    width: 100%
-  }
+	span {
+		position: relative;
+		display: inline-block;
+		width: 100%;
+	}
 `;
 
 export default BigHeader;

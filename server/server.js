d4247c1eac25bc583b068b018ba70e8b9ed4b59d
Ian Mostar <ian@rallyhr.com>
2020-05-26 19:36:52 +0800

    fix margin issue

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, "..", "build");
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-    res.sendFile( path.join(publicPath, 'index.html') );
- });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(publicPath, "index.html"));
+});
 
 app.listen(port, () => {
-    console.log(port)
+	console.log(port);
 });
